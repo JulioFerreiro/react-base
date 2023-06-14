097fcd016ba39c75fd0b007620183b50691c4361
Julio Ferreiro <julioferreiro@live.com>
2023-06-14 20:03:25 -0300

    Redux-Saga

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,16 +1,28 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log('Sucesso');
       const novoEstado = { ...state };
       novoEstado.botaoClicado = !novoEstado.botaoClicado;
       return novoEstado;
     }
-
-    default:
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Deu erro =(');
       return state;
+    }
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Estou fazendo a requesicão');
+      return state;
+    }
+
+    default: {
+      return state;
+    }
   }
 };
