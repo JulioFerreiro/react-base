6c2f8d864d66f74b8503f7ab8c6ce28d8f095647
Julio Ferreiro <julioferreiro@live.com>
2023-06-15 15:19:45 -0300

    LocalStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
+import { persistStore } from 'redux-persist';
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
+import persistedReducer from './modules/reduxPersist';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootsagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  persistedReducer(rootReducer),
+  applyMiddleware(sagaMiddleware)
+);
 
 sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(store);
 export default store;
